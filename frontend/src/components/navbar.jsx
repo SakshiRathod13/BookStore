ea4cd7eae96d15349e9bf30c93b099eb093d096e
SakshiRathod13 <sakshirathod84352@gmail.com>
2024-05-14 13:08:08 +0530

    Refactor navbar component for improved responsiveness and readability

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <div>
+    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
     <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
@@ -11,37 +11,29 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <li><a>Home</a></li>
+      <li><a>Courses</a></li>
+      <li><a>Contact</a></li>
+      <li><a>About</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">Book Store</a>
+    <a className="font-bold text-2xl">BookStore</a>
   </div>
+  <div className='navbar-end'>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Home</a></li>
+          <li><a>Courses</a></li>
+          <li><a>Contact</a></li>
+          <li><a>About</a></li>
+        
+        </ul>
+      </div>
+      <div className=''>
+        <a className="btn">Login</a>
+      </div>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
+     
 </div>
     </div>
     </>
