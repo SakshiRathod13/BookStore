cab93bb8daac27b71fffd9aeb24d720878d83ac6
SakshiRathod13 <sakshirathod84352@gmail.com>
2024-05-14 16:24:58 +0530

    Refactor navbar component for improved responsiveness and readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import Banner from './components/Banner'
+import Footer from './Footer'
 const App = () => {
   return (
     <div>  
       <Navbar/>
       <Banner/>
+      <Footer/>
     </div>
   )
 }
